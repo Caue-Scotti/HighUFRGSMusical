e1dd3aa937c5db5c6e7b09eea2aee3a6b44abb3a
Cauê Scotti <caue.scotti@gmail.com>
2023-12-19 02:16:52 -0300

    Up programa completo

---
+++ README.md
@@ -0,0 +1,4 @@
+# HighUFRGSMusical
+## Road to INFinity
+
+### Trabalho da disciplina de Técnicas de Construção de Programas da Universidade Federal do Rio Grande do Sul - UFRGS, feito pelos alunos Cauê Scotti, Galeano Folle, Luca Boni e Pedro Roewer
---
+++ src/main/implementacao/Teclado.java
@@ -0,0 +1,22 @@
+package implementacao;
+
+import com.raylib.Jaylib;
+import java.util.ArrayList;
+
+public class Teclado {
+
+    public static ArrayList<Tecla> leEntradas() {
+
+        ArrayList<Tecla> teclasPressionadas = new ArrayList<Tecla>();
+
+        for (int teclaCodificada = 0; teclaCodificada <= 346; teclaCodificada++) { // 346 é o maior código associado a uma tecla em Raylib
+            if (Jaylib.IsKeyDown(teclaCodificada)) {
+                Tecla tecla = new Tecla(teclaCodificada, true);
+                teclasPressionadas.add(tecla);
+            }
+        }
+
+        return teclasPressionadas;
+    }
+
+}
